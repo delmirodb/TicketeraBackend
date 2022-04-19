cd36dc75fea882b518c36ef1883f831678393b4e
Delmiro <delmirodb@gmail.com>
2022-04-20 00:25:40 +0200

    Tokens working

--- src/main/java/es/delmirodb/ticketerabackend/Service.java
+++ src/main/java/es/delmirodb/ticketerabackend/Service.java
@@ -1,10 +1,15 @@
 package es.delmirodb.ticketerabackend;
 
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.apache.tomcat.util.codec.binary.StringUtils;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class Service {
 
@@ -17,4 +22,19 @@ public class Service {
         return kf.generatePrivate(spec);
 
     }
+
+    public static PublicKey getPublic(String filename)
+            throws Exception {
+
+        byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
+
+        X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        return kf.generatePublic(spec);
+    }
+
+    public static String decodeToJson(final String base64){
+        return StringUtils.newStringUtf8(Base64.decodeBase64(base64));
+    }
+
 }
